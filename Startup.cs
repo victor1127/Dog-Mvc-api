30f58d02ac1cc5fc00cae64aeddb01ab7aa6d010
Victor Sosa <vsosa01127@gmail.com>
2022-05-16 19:37:26 -0400

    updating navbar

--- Startup.cs
+++ Startup.cs
@@ -54,7 +54,7 @@ namespace DogMvc
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Breeds}/{action=Index}/{id?}");
             });
         }
     }
