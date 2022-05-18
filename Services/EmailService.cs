afcbe781e5ad63205688be1faec15cc4d08fd20b
Victor Sosa <vsosa01127@gmail.com>
2022-05-17 20:34:45 -0400

    Creating EmailService

---
+++ Services/EmailService.cs
@@ -0,0 +1,54 @@
+﻿using DogMvc.Models.ConfigurationModels;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogMvc.Services
+{
+    public class EmailService : IEmailSender
+    {
+        private readonly EmailConfigurationModel _emailSettings;
+        public EmailService(IOptions<EmailConfigurationModel> emailSettings)
+        {
+            _emailSettings = emailSettings.Value;
+        }
+
+        public async Task SendEmailAsync(string name, string emailFrom, string subject, string htmlMessage)
+        {
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(_emailSettings.Email);
+            email.To.Add(MailboxAddress.Parse(_emailSettings.Email));
+            email.Subject = subject;
+
+            var body = new BodyBuilder()
+            {
+                HtmlBody = $"New message from:<br/><br/>" +
+                           $"Name: { name }<br/>" +
+                           $"Subject: { subject }<br/>" +
+                           $"<br/><br/>{ htmlMessage }"
+            };
+
+            email.Body = body.ToMessageBody();
+
+            var smtp = ConfigureSmtp();
+            await smtp.SendAsync(email);
+            smtp.Disconnect(true);
+        }
+
+        private SmtpClient ConfigureSmtp()
+        {
+            var smtp = new SmtpClient();
+            smtp.Connect(_emailSettings.Host, _emailSettings.Port, SecureSocketOptions.StartTlsWhenAvailable);
+            smtp.Authenticate(_emailSettings.Email, _emailSettings.Password);
+
+            return smtp;
+        }
+    }
+
+}
+
---
+++ Services/IEmailSender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogMvc.Services
+{
+    interface IEmailSender
+    {
+        Task SendEmailAsync(string name, string emailFrom, string subject, string htmlMessage);
+
+    }
+}
